cc1c30782da411072f787d77958f5f4c00b239f6
Yessworkinf <88673841+AraSancor@users.noreply.github.com>
2025-10-14 15:31:42 +0200

    yess

--- js/frase.js
+++ js/frase.js
@@ -1,13 +1,12 @@
-        // Texto que se mostrará letra por letra
+
         const textoCompleto = "Soy una profesional Web Developer con habilidades en gestión de contenido, diseño gráfico y marketing digital, respaldado por conocimientos en desarrollo backend y frontend. Mi enfoque integral me permite crear soluciones creativas y efectivas que impulsan la presencia digital y el éxito de proyectos diversos.";
 
-        // Elemento donde se mostrará el texto
         const contenedor = document.getElementById('texto');
 
-        // Configuración del efecto
-        const velocidad = 60; // milisegundos entre cada letra
+      
+        const velocidad = 60; 
 
-        // Función para mostrar el texto letra por letra
+    
         function tipeo(texto, paso = 0) {
           if (paso <= texto.length) {
             contenedor.textContent = texto.substring(0, paso);
@@ -15,7 +14,7 @@
           }
         }
 
-        // Iniciar el efecto cuando la página cargue
+        
         window.addEventListener('DOMContentLoaded', () => {
           tipeo(textoCompleto);
         });
